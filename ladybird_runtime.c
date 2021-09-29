415765e8588fa4ad0f855d62c815b0ee488f9cf6
harihitode <harihitode@gmail.com>
2021-09-29 23:46:13 +0900

    manage SIG_DFL, SIG_IGN properly

--- ladybird_runtime.c
+++ ladybird_runtime.c
@@ -1,7 +1,12 @@
 #include <signal.h>
+#include <stdlib.h>
 
 void _ladybird_trap_handler_c() {
 	void (*func)(int) = signal(SIGINT, SIG_DFL);
-	func(SIGINT);
+	if ((func != SIG_DFL) && (func != SIG_IGN)) {
+		func(SIGINT);
+	} else if (func == SIG_ERR) {
+		abort();
+	}
 	return;
 }
